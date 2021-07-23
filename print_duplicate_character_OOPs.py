6ce6f794b23079526ac84a95c3848bf34a04c499
Abhishek Singh <abhisheksinghaz@outlook.com>
2021-07-24 00:06:40 +0530

    updated with comments

--- print_duplicate_character_OOPs.py
+++ print_duplicate_character_OOPs.py
@@ -2,9 +2,13 @@
 
 class print_dupes:
   def __init__(self):
+    
+    # accepting the input from the user
     self.s = input("Enter a string: ")
 
   def output(self):
+    
+    # 'set(self.s)' will have all the characters of the entered string just once.
     for ele in set(self.s):
       if self.s.count(ele) > 1:
         print("count of " + ele + " = " + str(self.s.count(ele)))
