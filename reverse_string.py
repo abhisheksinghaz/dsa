c133a3c364ec9fb06f0383efb1c95ea1380cacd9
Abhishek Singh <abhisheksinghaz@outlook.com>
2021-07-23 23:28:53 +0530

    category: strings

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+# following line has the link to the problem statement
+# https://leetcode.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        
